42f9fab00c36657dbf750199227775dd6cc473d9
Shuja Ali <shujaali1234@gmail.com>
2023-10-13 04:17:24 +0500

    Billboard fixeS

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("104ad832-37e5-4682-b28c-97b813d781f0");
 
   return (
     <Container>
